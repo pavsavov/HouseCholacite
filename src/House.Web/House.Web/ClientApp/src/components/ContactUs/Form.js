9873b7c84d52fd70a2a3746007eb2745b6c105f8
Павел <pav.savov@gmail.com>
2020-10-26 22:35:43 +0200

    Form refactored.Removed repeative code for TextField component generation

--- src/House.Web/House.Web/ClientApp/src/components/ContactUs/Form.js
+++ src/House.Web/House.Web/ClientApp/src/components/ContactUs/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Button, TextField, Grid } from '@material-ui/core';
+import { Button, TextField, Grid, Unstable_TrapFocus } from '@material-ui/core';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import { v4 as uuidv4 } from 'uuid';
 
 const Form = (props) => {
   const initialInputState = {
@@ -33,46 +34,49 @@ const Form = (props) => {
     );
   };
 
-  //create new component => styled TextField and use some map or other iterational function
+  const textFieldPropsAndValues = [
+    {
+      id: uuidv4(),
+      label: 'Име',
+      name: 'FirstName',
+      value: FirstName,
+      onChange: handleInputChange,
+      className: props.style.styleTextField,
+    },
+    {
+      id: uuidv4(),
+      label: 'Фамилия',
+      name: 'LastName',
+      value: LastName,
+      onChange: handleInputChange,
+      className: props.style.styleTextField,
+    },
+    {
+      id: uuidv4(),
+      label: 'Телефонен номер',
+      name: 'PhoneNumber',
+      value: PhoneNumber,
+      onChange: handleInputChange,
+      className: props.style.styleTextField,
+    },
+    {
+      id: uuidv4(),
+      label: 'Електронна поща',
+      name: 'Email',
+      value: Email,
+      onChange: handleInputChange,
+      className: props.style.styleTextField,
+    },
+  ];
+
   return (
     <form>
       <Grid container onSubmit={submitHandler} direction='column'>
-        {/* <Grid container direction='column'> */}
         <Grid item>
-          <TextField
-            label='Име'
-            name='FirstName'
-            value={FirstName}
-            onChange={handleInputChange}
-            className={props.style.styleTextField}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            label='Фамилия'
-            name='LastName'
-            value={LastName}
-            onChange={handleInputChange}
-            className={props.style.styleTextField}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            label='Телефон за връзка'
-            name='PhoneNumber'
-            value={PhoneNumber}
-            onChange={handleInputChange}
-            className={props.style.styleTextField}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            label='Електронна поща'
-            name='Email'
-            value={Email}
-            onChange={handleInputChange}
-            className={props.style.styleTextField}
-          />
+          {textFieldPropsAndValues.map((textFieldProps) => {
+            const { id, ...propsAndValues } = textFieldProps;
+            return <TextField key={id} {...propsAndValues} />;
+          })}
         </Grid>
         <Grid item>
           <TextareaAutosize
